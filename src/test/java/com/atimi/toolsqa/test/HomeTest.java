6c4f97e76de70257d4bc77e8227a6e856366dc79
dbshivanagoudra <dbshivanagoudra@atimi.com>
2024-05-16 12:39:17 +0530

    Updated 'testHomeLink' and 'testLogo' test methods.

--- src/test/java/com/atimi/toolsqa/test/HomeTest.java
+++ src/test/java/com/atimi/toolsqa/test/HomeTest.java
@@ -6,17 +6,18 @@ import org.testng.annotations.Test;
 
 public class HomeTest extends BaseTest {
 
-    @Test(priority = 0)
+    @Test(priority = 0, description = "Verify tools qa logo ids displayed.")
     public void testLogo() {
         getHome().waitForScreenToLoad();
         Assert.assertTrue(getHome().isLogoDisplayed(), "Tools QA Logo is not displayed.");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 1, description = "Verify selenium training page is displayed.")
     public void testHomeLink() {
         getHome().waitForScreenToLoad();
         getHome().clickSeleniumTraining();
         Assert.assertTrue(getHome().isSeleniumTrainingPageDisplayed(),
                 "Selenium Training page is not displayed.");
     }
+
 }
